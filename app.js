f314d651cbae25cfb3aa40e5b4a7344bbfa7db18
Musa Aly <120222011+MusaAli786@users.noreply.github.com>
2023-05-13 15:23:19 -0700

    video play and pause

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const videoMov = document.querySelector('.vid'); 
+ 
+
+
+
+
+const playBtn = ()=>{
+    videoMov.play();
+}
+
+
+const pauseBtn = ()=>{
+    videoMov.pause();
+}
+// console.log(pauseBtn)
